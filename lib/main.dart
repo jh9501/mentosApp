73d3a34aff225c65691a20a51fdbb2519fe01cb6
jh9501 <wogns0579@naver.com>
2023-06-09 21:57:41 +0900

    프론트 최종 업데이트
    23-06-09

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mentos/page/login_page.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting();
+
   runApp(
     MaterialApp(
         home: login_page()
--- lib/page/main_study.dart
+++ lib/page/main_study.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:mentos/main.dart';
+import 'package:mentos/navigation_screen.dart';
 
 class Post1 {
   final String title;
-  final String content;
   final String author;
 
   Post1({
     required this.title,
-    required this.content,
     required this.author,
   });
 }
@@ -20,22 +20,26 @@ class MainStudy extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
+  void navigateToPostDetail(BuildContext context, Post1 post) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => RootScreen()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     List<Post1> posts = [
       Post1(
         title: 'C언어 스터디',
-        content: '(공지사항)',
         author: '2/4',
       ),
       Post1(
         title: '프론트엔드(React) 스터디',
-        content: '매주 월요일, 금요일 저녁 7시부터 10시까지~',
         author: '3/8',
       ),
       Post1(
         title: '정보처리기사 자격증 공부',
-        content: '주말마다 1챕터씩 공부해오기!!',
         author: '4/5',
       ),
     ];
@@ -53,54 +57,49 @@ class MainStudy extends StatelessWidget {
           itemCount: posts.length,
           itemBuilder: (context, index) {
             Post1 post = posts[index];
-            return Container(
-              margin: EdgeInsets.all(10),
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    post.title,
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+            return InkWell(
+              onTap: () => navigateToPostDetail(context, post),
+              child: Container(
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
                     ),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    post.content,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.black87,
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  Row(
-                    children: [
-                      Icon(Icons.person, size: 15),
-                      SizedBox(width: 4),
-                      Text(
-                        post.author,
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.grey,
-                        ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      post.title,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
-                  ),
-                ],
+                    ),
+                    SizedBox(height: 12),
+                    Row(
+                      children: [
+                        Icon(Icons.person, size: 15),
+                        SizedBox(width: 4),
+                        Text(
+                          post.author,
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             );
           },
